674e018ac2e5f3bf84f9cc3b6bb8910c17dc8196
Dakota <dakota742009@gmail.com>
2024-04-19 18:06:49 +0200

    #4 placing elements

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,21 @@
 import '../css/home.css';
 import TrackList from '../components/trackList';
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SpotifyFetchedAlbumModel from '../models/spotifyFetchedAlbumModel';
 
 function Home() {
   const [albumData, setAlbumData] = useState<SpotifyFetchedAlbumModel>()
+  const [search, setSearch] = useState('')
   const headerRef = useRef(null)
+
+  function handleSearchChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value)
+  }
+
+  function handleSearchSubmit(event: React.FormEvent) {
+    event.preventDefault()
+    // search func
+  }
 
   useEffect(() => {
     if (headerRef.current) {
@@ -24,7 +34,17 @@ function Home() {
   return (
     <div className='home'>
       <header ref={headerRef}>
+        <div></div>
         <h1>{albumData?.name}</h1>
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            value={search}
+            onChange={handleSearchChange}
+            placeholder='Search another album'
+          />
+          <button type="submit">Search</button>
+        </form>
       </header>
       <TrackList
         setAlbumData={setAlbumData}
